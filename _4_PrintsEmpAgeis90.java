ff7289df3d93dd5c976eeabebeef7dddcbf64e5b
Sakthi_Official <sakthiveld479@gmail.com>
2024-09-18 17:59:30 +0530

    18/09/2024
    The Employee age is greater than 90 so print you are not eligible to work

---
+++ _4_PrintsEmpAgeis90.java
@@ -0,0 +1,18 @@
+
+
+public class _4_PrintsEmpAgeis90 {
+
+	public static void main(String[] args) {
+
+		String Empname="Ramana";
+		int Empage=30;
+		
+		if(Empage>=90) 
+			System.out.println(Empname+" - You are Not Eligibe to Work in a Company :(");
+		else 
+			System.out.println(Empname+" - You are Eligible to Work in a Company :)");
+		
+		
+	}
+
+}
