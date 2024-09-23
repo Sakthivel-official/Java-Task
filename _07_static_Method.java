6919d41c3b72b11cd8a7982e7e0304acbf441351
Sakthivel_D <sakthiveld479@gmail.com>
2024-09-23 12:36:20 +0530

    Date: 23/09/2024

---
+++ _07_static_Method.java
@@ -0,0 +1,31 @@
+
+public class _07_static_Method {
+	
+	//Q1. to print add two numbers using static method 
+	//Q2. to print two numbers using public method
+	
+	static int a ;
+	static int b;
+	
+	static void StaticAdd() {
+		System.out.println("Static Method -> Addition of : "+a+" and "+b+" is: "+(a+b));
+	}
+
+	public void Add() {
+		System.out.println("public Method -> Addition of : "+a+" and "+b+" is: "+(a+b));
+	}
+	public static void main(String[] args) {
+
+		
+		a = 22; // a,b is static variable we can directly access the variables  
+		b = 30;
+		
+		StaticAdd();  // access the static method directly
+		
+		_07_static_Method add = new _07_static_Method(); //creating a object for class to access non-static method
+		
+		add.Add(); //access the public method using object
+		
+	}
+
+}
