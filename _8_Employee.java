32c6125b82f07ef4a2556d3e132e7c4b092c04c5
Sakthivel_D <sakthiveld479@gmail.com>
2024-09-24 20:28:10 +0530

    Date: 24/09/2024

--- _8_Employee.java
+++ _8_Employee.java
@@ -1,7 +1,5 @@
 
 //1. write a program to print employee details using constructor(empid,empname,empsal,empage,empdistioga);
-//2. create a class dog with instance variable name,color. implement a parameterized that takes name,color as parameter and initialize the instance variable
-//   print the values the variable
 public class Employee {
 	int empid;
 	String empname;
