eb4a461aaa913386fb068330c4967c88f4980a27
Sakthivel_D <sakthiveld479@gmail.com>
2024-09-25 21:39:38 +0530

    to calculate Area of rectangle , Peremeter

--- _9_Rectangle.java
+++ _9_Rectangle.java
@@ -1,8 +1,6 @@
 
 /*3. Write a Java program to create a class called "Rectangle"   
       with width and height attributes. Calculate the area and perimeter of the rectangle.
-
-  
 */
 
 public class Rectangle {
